6f87f6f598cd1225e5bd714007984fa984a085c0
Defaillances <mathieu.son@live.fr>
2021-11-05 09:44:32 +0100

    [+] add and update consumer, producer and docker file [+]

--- producer.py
+++ producer.py
@@ -5,7 +5,7 @@ import time
 import requests
 
 d = requests.get("https://data.messari.io/api/v1/assets?fields=id,slug,symbol,metrics/market_data/price_usd").json()
-print(d)
-p = KafkaProducer(bootstrap_servers=['localhost:9092'])
-p.send('test6', json.dumps(d).encode('utf-8'))
+p = KafkaProducer(bootstrap_servers='localhost:9092')
+p.send('test7', json.dumps(d).encode('utf-8'))
 p.flush()
+print(d)
